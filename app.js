b52b540dae5ab34cfdb5c65996552b7bfb495f1d
mohammad mahdi <moha00ad.mahdi@gmail.com>
2025-08-25 15:18:17 +0330

    Finish full layout and interaction – ready for testing

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 "use strict";
-//VARIABLES
-window.addEventListener("keydown", (e) => {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`.key[data-key="${e.code}"]`);
+    if (!key || !audio)
+        return;
     audio.currentTime = 0;
     audio.play();
-});
+    key.classList.add("active");
+}
+function removeActive(e) {
+    if (e.propertyName !== "transform")
+        return;
+    e.target.classList.remove("active");
+}
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach(key => key.addEventListener("transitionend", removeActive));
+window.addEventListener("keydown", playSound);
--- app.ts
+++ app.ts
@@ -1,8 +1,21 @@
-//VARIABLES
+function playSound(e: KeyboardEvent) {
+  const audio = document.querySelector(`audio[data-key="${e.code}"]`) as HTMLAudioElement;
+  const key = document.querySelector(`.key[data-key="${e.code}"]`);
 
-window.addEventListener("keydown", (e) => {
-    const audio = document.querySelector(`audio[data-key="${e.code}"]`) as HTMLAudioElement;
+  if (!key || !audio) return;
 
-    audio.currentTime = 0;
-    audio.play();
-});
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("active");
+}
+
+function removeActive(e: TransitionEvent) {
+    if (e.propertyName !== "transform") return;
+  (e.target as HTMLElement).classList.remove("active");
+}
+
+
+const keys = Array.from(document.querySelectorAll(".key")) as HTMLElement[];
+keys.forEach(key => key.addEventListener("transitionend", removeActive));
+
+window.addEventListener("keydown", playSound);
